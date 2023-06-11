c0119271ed614ff8284c156fc7de2fce7232b2ec
RubelAzad <rubelazad123@gmail.com>
2023-06-11 17:08:02 +0600

    patient view and edit

---
+++ Modules/Patient/Entities/District.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Patient\Entities;
+use Modules\Patient\Entities\Address;
+use Modules\Base\Entities\BaseModel;
+
+class District extends BaseModel
+{
+    protected $table = 'District';
+    public $timestamps = false;
+
+    
+}
