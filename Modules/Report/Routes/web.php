92009e19546d7174fab2d090fd09bfaf91394541
mihalazmaeen <azmaeenmihal1@gmail.com>
2023-07-05 16:57:34 +0600

    glucose report added with fbg,rbg,hbg

--- Modules/Report/Routes/web.php
+++ Modules/Report/Routes/web.php
@@ -16,15 +16,17 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('datewisedx', 'ReportController@datewisedxindex')->name('datewisedx');
     Route::any('search-by-age', 'ReportController@SearchByAge')->name('search-by-age');
     Route::any('date-wise-dx', 'ReportController@SearchByDate')->name('date-wise-dx');
+    Route::get('glucosegraph', 'ReportController@glucosegraphindex')->name('glucosegraph');
+    Route::post('glucose-graph', 'ReportController@GlucoseGraph')->name('glucose-graph');
     Route::get('patient-blood-pressure-graph', 'ReportController@PatientBloodPressureGraph')->name('patientbloodpressuregraph');
     Route::get('ajax-patient-blood-pressure', 'ReportController@AjaxPatientBloodPressure')->name('ajaxpatientbloodpressure');
-    
+
     Route::get('heart-rate-graph','ReportController@HeartRateGraph')->name('heartrategraph');
     Route::get('ajax-heart-rate-graph','ReportController@AjaxHeartRateGraph')->name('ajaxheartrategraph');
-    
+
     Route::get('temperature-graph','ReportController@TemperatureGraph')->name('temperaturegraph');
     Route::get('ajax-temperature-graph','ReportController@AjaxTemperatureGraph')->name('ajaxtemperaturegraph');
-    
+
     Route::group(['prefix' => 'patientage', 'as'=>'patientage.'], function () {
         Route::post('show', 'ReportController@show')->name('show');
     });
